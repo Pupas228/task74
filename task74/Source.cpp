6242b69d2174d4b1005ff327e28be042d2da4c34
milena0000 <kalistratovamilena00@gmail.com>
2023-06-14 18:44:51 +0300

    Add project files.

---
+++ task74/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ofstream fout("jesus.txt");
+	
+
+
+	if (int file1, int file2) {
+		file1 < file2 ? ;
+		return ;
+	}
+	else {
+		return 0;
+	}
+}
